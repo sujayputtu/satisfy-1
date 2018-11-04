f7dcb3e048f9fedd1df54d31fcbe3d90f71accef
VadirajaRao <vadiraja98rao@gmail.com>
2018-11-04 20:23:57 +0530

    Signup page completed

--- insert.py
+++ insert.py
@@ -21,16 +21,40 @@ class insert_val(object):
 
     def insert_user_mail (self, uid, mail):
         """Insert values into user_mail table."""
-        pass
+        sql = 'insert into user_mail(uid, mail) values (%s, %s)'
+        val = (uid, mail)
+
+        self.cur.execute(sql, val)
+
+        return
 
     def insert_user_age (self, uid, age):
         """Insert value into user_age table."""
-        pass
+        sql = 'insert into user_age(uid, age) values (%s, %s)'
+        val = (uid, age)
+
+        self.cur.execute(sql, val)
+
+        return
 
     # This function is yet to be completed.
     def insert_user(self, *args):
         """Insert values into user table."""
-        pass
+        sql = 'insert into user(fname, lname, passwd, dob, city, height, weight, tot_dist, tot_time) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        val = (args[1], args[2], args[3], args[4], args[5], args[6], args[7], 0.0, 0.0)
+
+        self.cur.execute(sql, val)
+
+        self.cur.execute('select max(uid) from user')
+        uid = self.cur.fetchone()
+
+        self.insert_user_mail(uid[0], args[0])
+
+        dob = datetime.datetime.strptime(args[4], "%Y-%m-%d")
+        age = self.calculate_age(dob)
+        self.insert_user_age(uid, age)
+
+        self.sat.commit()
 
     def insert_friend(self, *args):
         """Insert values into friend table."""
